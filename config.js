879e8ed6ad0b83df82e87544c58fc52df3896e83
Jamen Lyndon <jamenlyndon@everydayhero.com>
2015-08-06 09:35:52 +1000

    Dev

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = [
   {
     name: "la-marathon",
     campaignUids: [
-      "64ebc330-501e-4e5f-98ba-54bb09e7a158"
+      "64ebc330-501e-4e5f-98ba-54bb09e7a158",
+      'us-22'
     ]
   }
 ];
